b7d85b861dedc013669b653db3ca7f63f2af92df
Simon Ranftl <s.ranftl@netconomy.net>
2021-04-05 20:27:39 +0200

    added env options

--- src/main/java/com/springreact/template/SpringReactOAuthApplication.java
+++ src/main/java/com/springreact/template/SpringReactOAuthApplication.java
@@ -1,12 +1,33 @@
 package com.springreact.template;
 
-import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.StandardEnvironment;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class SpringReactOAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringReactOAuthApplication.class, args);
+
+		Map<String, Object> env = Dotenv.load()
+				.entries()
+				.stream()
+				.collect(
+						Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
+		new SpringApplicationBuilder(SpringReactOAuthApplication.class)
+				.environment(new StandardEnvironment() {
+					@Override
+					protected void customizePropertySources(MutablePropertySources propertySources) {
+						super.customizePropertySources(propertySources);
+						propertySources.addLast(new MapPropertySource("dotenvProperties", env));
+					}
+				}).run(args);
 	}
 }
